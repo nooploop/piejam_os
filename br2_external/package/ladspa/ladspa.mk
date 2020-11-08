02e13ba06a80bea7f91cce18d935e39ce8b792c8
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2020-11-08 23:30:09 +0100

    Add LADSPA package

---
+++ br2_external/package/ladspa/ladspa.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# LADSPA
+#
+################################################################################
+
+LADSPA_VERSION = 1.15
+LADSPA_SOURCE = ladspa_sdk_$(LADSPA_VERSION).tgz
+LADSPA_SITE = http://www.ladspa.org/download
+LADSPA_INSTALL_STAGING = YES
+LADSPA_INSTALL_TARGET = NO
+LADSPA_LICENSE = LGPL-2.1-only
+
+define LADSPA_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/src/ladspa.h $(STAGING_DIR)/usr/include/ladspa.h
+endef
+
+$(eval $(generic-package))
