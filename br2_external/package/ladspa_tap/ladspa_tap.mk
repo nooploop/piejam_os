4fa17c1237399304f40f584d897cf5bb7f136623
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2020-12-31 14:41:49 +0100

    Add LADSPA TAP plugins package

---
+++ br2_external/package/ladspa_tap/ladspa_tap.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# LADSPA_TAP
+#
+################################################################################
+
+LADSPA_TAP_VERSION = v1.0.1
+LADSPA_TAP_SITE = https://github.com/tomszilagyi/tap-plugins
+LADSPA_TAP_SITE_METHOD = git
+LADSPA_TAP_LICENSE = GPL-2.0+
+
+define LADSPA_TAP_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define LADSPA_TAP_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/*.so $(TARGET_DIR)/usr/lib/ladspa/
+endef
+
+$(eval $(generic-package))
