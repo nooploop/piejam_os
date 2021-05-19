5b7f42e483d15755940ce8dd96a698a42da43f14
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2021-05-19 19:42:01 +0200

    Add license information for custom packages

--- br2_external/package/ladspa/ladspa.mk
+++ br2_external/package/ladspa/ladspa.mk
@@ -10,6 +10,7 @@ LADSPA_SITE = http://www.ladspa.org/download
 LADSPA_INSTALL_STAGING = YES
 LADSPA_INSTALL_TARGET = NO
 LADSPA_LICENSE = LGPL-2.1-only
+LADSPA_LICENSE_FILES = doc/COPYING
 
 define LADSPA_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/src/ladspa.h $(STAGING_DIR)/usr/include/ladspa.h
