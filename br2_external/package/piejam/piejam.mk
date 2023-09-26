f23b91905700040564cb861ac74a2653bd779b19
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2023-09-26 20:36:57 +0200

    update to PieJam v0.8.1

--- br2_external/package/piejam/piejam.mk
+++ br2_external/package/piejam/piejam.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PIEJAM_VERSION = v0.8.0
+PIEJAM_VERSION = v0.8.1
 PIEJAM_SITE = https://github.com/nooploop/piejam.git
 PIEJAM_SITE_METHOD = git
 PIEJAM_GIT_SUBMODULES = YES
